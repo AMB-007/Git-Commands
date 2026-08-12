e845bf2aef94101913ae2abeefe2ae219fd67e85
AMB-007 <arjunmbabu777@gmail.com>
2026-08-12 09:50:06 +0530

    1st commit

---
+++ sample.py
@@ -0,0 +1,23 @@
+"""
+Git commands
+=====================
+git status >>> to know the current staus of or files
+git init >>> to create new git repository (initializing git)
+
+
+git add .  >>> add all changed files(untracked) into staging area(ready to commit)
+
+git add sample.py (to add only one file "sample.py" into the staging area)
+
+git commit -m "sample modules created"  >>>  save the change permentantely in git with message
+
+git log >>> helps to get the history(all commits)
+"""
+
+
+"""
+Bonous commands
+
+
+
+"""
